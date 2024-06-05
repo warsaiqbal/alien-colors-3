1050883ab9e070bb8a565f131d543af6a1d04603
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:22:03 -0700

    Add files via upload

---
+++ 26. Alien Colors #3/main.js
@@ -0,0 +1,42 @@
+//Runs if block
+var alien_color = "green";
+if (alien_color == 'green') {
+    console.log("You earned 5 points!");
+}
+else if (alien_color == 'yellow') {
+    console.log("You earned 10 points!");
+}
+else if (alien_color == 'red') {
+    console.log("You earned 15 points!");
+}
+else {
+    console.log("You earned 0 points.");
+}
+//Runs else if block1
+var alien_color2 = "yellow";
+if (alien_color2 == 'green') {
+    console.log("You earned 5 points!");
+}
+else if (alien_color2 == 'yellow') {
+    console.log("You earned 10 points!");
+}
+else if (alien_color2 == 'red') {
+    console.log("You earned 15 points!");
+}
+else {
+    console.log("You earned 0 points.");
+}
+//Runs else if block2
+var alien_color3 = "red";
+if (alien_color3 == 'green') {
+    console.log("You earned 5 points!");
+}
+else if (alien_color3 == 'yellow') {
+    console.log("You earned 10 points!");
+}
+else if (alien_color3 == 'red') {
+    console.log("You earned 15 points!");
+}
+else {
+    console.log("You earned 0 points.");
+}
---
+++ 26. Alien Colors #3/main.ts
@@ -0,0 +1,38 @@
+//Runs if block
+let alien_color: string = "green"
+
+if (alien_color == 'green'){
+    console.log("You earned 5 points!");
+} else if (alien_color == 'yellow'){
+    console.log("You earned 10 points!");
+} else if (alien_color == 'red'){
+    console.log("You earned 15 points!");
+} else{
+    console.log("You earned 0 points.");
+}
+
+//Runs else if block1
+
+let alien_color2: string = "yellow"
+if (alien_color2 == 'green'){
+    console.log("You earned 5 points!");
+} else if (alien_color2 == 'yellow'){
+    console.log("You earned 10 points!");
+} else if (alien_color2 == 'red'){
+    console.log("You earned 15 points!");
+} else{
+    console.log("You earned 0 points.");
+}
+
+//Runs else if block2
+
+let alien_color3: string = "red"
+if (alien_color3 == 'green'){
+    console.log("You earned 5 points!");
+} else if (alien_color3 == 'yellow'){
+    console.log("You earned 10 points!");
+} else if (alien_color3 == 'red'){
+    console.log("You earned 15 points!");
+} else{
+    console.log("You earned 0 points.");
+}
